5580b7af27f1cd5eeb54258770bd88a806b9cfcb
Aurore Collet <auroreclt@gmail.com>
2026-02-13 12:48:49 +0100

    mise en place du formulaire ok avec gestion des erreurs côté serveur et re-remplissage des champs corrects

--- src/controllers/shop.controller.js
+++ src/controllers/shop.controller.js
@@ -4,26 +4,42 @@ const shopController = {
   },
 
   thanksPage: (req, res, next) => {
-    res.render("thankyou", { style: "home" });
+    res.render("thankyou", { style: "shop" });
   },
 
   formPart: (req, res, next) => {
-    const { first_name, last_name, email, message } = req.body;
-    console.log(first_name);
+    const contactData = req.body;
+    console.log(contactData);
     // Check informations format
     const errors = {};
     let isOk = true;
 
-    if (!first_name) {
+    if (!contactData.first_name) {
       isOk = false;
       errors.first_name = "Le prénom est obligatoire";
     }
-    if (!last_name) {
+    if (!contactData.last_name) {
       isOk = false;
       errors.last_name = "Le nom est obligatoire";
     }
-
-    res.redirect("/boutique/merci");
+    if (!contactData.email) {
+      isOk = false;
+      errors.email = "L'email est obligatoire au format nom@mail.com";
+    }
+    if (!contactData.message || contactData.message.length > 1500) {
+      isOk = false;
+      errors.message =
+        "Le message est obligatoire et doit faire au maximum 1 500 caractères";
+    }
+    if (!isOk) {
+      res.render("shop", {
+        style: "shop",
+        data: contactData,
+        errors,
+      });
+    } else {
+      res.redirect("/boutique/merci#contact-form");
+    }
   },
 };
 
